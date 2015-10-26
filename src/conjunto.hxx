110be66976b8a32a7071a61d5ff2544ff5025783
saytes <saytes@correo.ugr.es>
2015-10-26 18:56:56 +0100

    primeras ediciones de los constructores en conjunto(sin terminar)

--- src/conjunto.hxx
+++ src/conjunto.hxx
@@ -4,23 +4,42 @@
   
 conjunto::conjunto( ){
 	
+	entrada;
+	size_type=0;
 		
-
+	
 	
 
 //Constructor de copia
 	
 conjunto::conjunto (const conjunto & d){
+
+	entrada = d.entrada;
+	size_type = d.size_type;
+
+}
+
 pair<conjunto::entrada,bool>  findID( const long int & id) const;
+
+
 conjunto<conjunto::entrada>  findIUCR( const string & iucr) const;
+
 conjunto<conjunto::entrada>  findDESCR( const string & descr) const;
+
 bool insert( const conjunto::entrada & e);
+
 bool erase(const long int & id);
+
 bool erase(const  conjunto::entrada & e);
+
 conjunto & operator=( const conjunto & org);
+
 size_type size() const ;
+
 bool empty() const;
+
 bool cheq_rep( ) const;
+
 ostream &  operator << ( ostream & sal, const conjunto & D);
 
 #endif
